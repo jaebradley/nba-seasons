c211fcbec452c8c087a024b32a937d06552fbe8a
Jae Bradley <jae.b.bradley@gmail.com>
2022-12-24 12:07:14 -0500

    Reorganize models

--- src/parsing/com/basketball_reference/franchises.py
+++ src/parsing/com/basketball_reference/franchises.py
@@ -6,40 +6,6 @@ from html.parser import HTMLParser
 from typing import Callable
 
 
-@dataclass(frozen=True)
-class Team:
-    name: str
-    first_season_start_year: int
-    last_season_start_year: int
-
-    def __post_init__(self):
-        if 0 >= self.first_season_start_year:
-            raise ValueError("First season start year must be positive")
-
-        if 0 >= self.last_season_start_year:
-            raise ValueError("Last season start year must be positive")
-
-        if 0 >= len(self.name):
-            raise ValueError("Name cannot be empty")
-
-
-@dataclass(frozen=True)
-class Franchise:
-    name: str
-    first_season_start_year: int
-    last_season_start_year: int
-
-    def __post_init__(self):
-        if 0 >= self.first_season_start_year:
-            raise ValueError("First season start year must be positive")
-
-        if 0 >= self.last_season_start_year:
-            raise ValueError("Last season start year must be positive")
-
-        if 0 >= len(self.name):
-            raise ValueError("Name cannot be empty")
-
-
 @dataclass(unsafe_hash=True)
 class Record:
     class Type(enum.Enum):
---
+++ src/parsing/com/nba/models.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Team:
+    name: str
+    first_season_start_year: int
+    last_season_start_year: int
+
+    def __post_init__(self):
+        if 0 >= self.first_season_start_year:
+            raise ValueError("First season start year must be positive")
+
+        if 0 >= self.last_season_start_year:
+            raise ValueError("Last season start year must be positive")
+
+        if 0 >= len(self.name):
+            raise ValueError("Name cannot be empty")
+
+
+@dataclass(frozen=True)
+class Franchise:
+    name: str
+    first_season_start_year: int
+    last_season_start_year: int
+
+    def __post_init__(self):
+        if 0 >= self.first_season_start_year:
+            raise ValueError("First season start year must be positive")
+
+        if 0 >= self.last_season_start_year:
+            raise ValueError("Last season start year must be positive")
+
+        if 0 >= len(self.name):
+            raise ValueError("Name cannot be empty")
