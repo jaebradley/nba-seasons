6ee9ffd2ca0f4f77bedec3a3f586e917185e860f
Jae Bradley <jae.b.bradley@gmail.com>
2022-12-26 21:49:51 -0500

    Simplify (#7)

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import os.path
 from pathlib import Path
 
+from src.parsing.com.basketball_reference.franchises import FranchiseAndTeamParser
 from src.parsing.com.basketball_reference.seasons import parse as parse_seasons
-from src.parsing.com.nba.franchises import parse as parse_franchises
+from src.parsing.com.nba.franchises import Parser, RecordHandler
 from src.translators.com.basketball_reference.franchise import translate_franchise, filter_franchises
 from src.translators.com.basketball_reference.league import translate_league
 from src.writers.com.basketball_reference.seasons import LeagueWriter
@@ -10,7 +11,13 @@ from src.writers.com.basketball_reference.seasons import LeagueWriter
 
 def main():
     with open("../src/data/com/basketball_reference/teams/2022-06-22.html", "r") as franchises_file:
-        franchise_history = parse_franchises(franchises_file.read())
+        records = list()
+        parser = FranchiseAndTeamParser(lambda record: records.append(record))
+        parser.feed(data=franchises_file.read())
+        parser.close()
+        p = Parser(
+            record_handler=RecordHandler(start_year_deserializer=lambda start_year: int(start_year.split("-")[0])))
+        franchise_history = p.parse(records=records)
 
         with open("../src/data/com/basketball_reference/seasons/2022-05-20.html", "r") as seasons_file:
             seasons = parse_seasons(seasons_file.read())
--- src/parsing/com/nba/test_franchises.py
+++ src/parsing/com/nba/test_franchises.py
@@ -1,12 +1,19 @@
 from unittest import TestCase
 
-from franchises import parse
+from franchises import Parser, RecordHandler
+from src.parsing.com.basketball_reference.franchises import FranchiseAndTeamParser
 
 
 class Test(TestCase):
     def test_parse(self):
         with open("/Users/jaebradley/projects/nba-seasons/src/data/com/basketball_reference/teams/2022-06-22.html",
                   "r") as file:
-            history = parse(file.read())
+            records = list()
+            parser = FranchiseAndTeamParser(lambda record: records.append(record))
+            parser.feed(data=file.read())
+            parser.close()
+
+            parser = Parser(record_handler=RecordHandler(lambda start_year: int(start_year.split("-")[0])))
+            history = parser.parse(records=records)
 
             self.assertIsNotNone(history)
